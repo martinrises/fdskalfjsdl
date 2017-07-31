55bb84dc45fd3819457efc3de9ed0b64ed42d168
lzf <lzfupdate@163.com>
2017-07-31 22:54:22 +0800

    用label_price.py给每日价格添加标签

--- label_price.py
+++ label_price.py
@@ -1,24 +1,25 @@
-target_file = open("labeled_daily_price.csv", 'w')
+import math
+
+
 trend_down = 0
 trend_shake = 1
 trend_up = 2
 meta_trend_days = 5
+rate_threshold = 0.2
 
 class DailyRecord:
     def __init__(self, open, close, high, low, turnover, volume):
-        self.open = open
-        self.close = close
-        self.high = high
-        self.low = low
-        self.turnover = turnover
-        self.volume = volume
+        self.open = float(open)
+        self.close = float(close)
+        self.high = float(high)
+        self.low = float(low)
+        self.turnover = float(turnover)
+        self.volume = float(volume)
 
-def line2DailyRecord(line, open_ix, close_ix, high_ix, low_ix, turnover_ix, volume_ix):
-    words = line.strip().split(",")
-    return 
 
+target_file = open("labeled_daily_price.csv", 'w')
 with open("daily_price.csv", "r") as src_file:
-    lines = src_file.readlines()
+    lines = src_file.readlines()	
 
     # ,open,close,high,low,total_turnover,volume
     keys = lines[0].strip().split(',')
@@ -39,3 +40,22 @@ with open("daily_price.csv", "r") as src_file:
         words = line.strip().split(",")
         records.append(DailyRecord(words[open_ix], words[close_ix], words[high_ix], words[low_ix], words[turnover_ix], words[volume_ix]))
 
+
+    # for i in range(meta_trend_days + 1, len(records)):
+    for i in range(0, len(lines) - meta_trend_days + 1):
+    	diff_price = records[i + meta_trend_days - 1].close - records[i].close
+    	highest = 0
+    	lowest = 9999999
+    	for ix in range(i, i + meta_trend_days):
+    		highest = max(highest, records[ix].high)
+    		lowest = min(lowest, records[ix].low)
+    	threshold_price = (highest - lowest) * rate_threshold
+    	line = lines[i].strip()
+    	if diff_price >= threshold_price:
+    		target_file.write(line + "," + str(trend_up) +"\n")
+    	elif diff_price <= -threshold_price:
+    		target_file.write(line + "," + str(trend_down) +"\n")
+    	else:
+    		target_file.write(line + "," + str(trend_shake) +"\n")
+
+target_file.close()
