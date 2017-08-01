b2d1f2ff919dc145e19076c03a5513c3a0082b29
lzf <lzfupdate@163.com>
2017-08-02 00:20:59 +0800

    参考github上的一份代码，可以用我想要的数据运行起来了

--- toothless.py
+++ toothless.py
@@ -1,122 +1,141 @@
-from __future__ import print_function, division
-import numpy as np
-import tensorflow as tf
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
-feature_size = 6
-num_epochs = 100
-total_series_length = 50000
-truncated_backprop_length = 15
-state_size = 4
-num_classes = 2
-echo_step = 3
-batch_size = 5
-num_batches = total_series_length//batch_size//truncated_backprop_length
-
-def generateData():
-    #――――――――――――――――――导入数据――――――――――――――――――――――
-    f=open('labeled_daily_price.csv') 
-    df=pd.read_csv(f)     #读入股票数据
-    x = df.iloc[:, 1:7]
-    y = df.iloc[:, 7:8]
-
-    x = np.asarray(x)
-    y = np.asarray(y)
-
-    x = x.reshape((batch_size, -1, feature_size))  # The first index changing slowest, subseries as rows
-    y = y.reshape((batch_size, -1)) # 将数组变形为batch_size行，总维数/行数 列的数组
-
-    return (x, y)
-
-batchX_placeholder = tf.placeholder(tf.float32, [batch_size, truncated_backprop_length, feature_size])
-batchY_placeholder = tf.placeholder(tf.int32, [batch_size, truncated_backprop_length])
-
-init_state = tf.placeholder(tf.float32, [batch_size, state_size])
-
-W = tf.Variable(np.random.rand(state_size+1, state_size), dtype=tf.float32)
-b = tf.Variable(np.zeros((1,state_size)), dtype=tf.float32)
-
-W2 = tf.Variable(np.random.rand(state_size, num_classes),dtype=tf.float32)
-b2 = tf.Variable(np.zeros((1,num_classes)), dtype=tf.float32)
-# Unpack columns
-
-inputs_series = tf.unstack(batchX_placeholder, axis=1) # 过时方法，tf.unstack()代替，axis = 1 -> 按列解包
-labels_series = tf.unstack(batchY_placeholder, axis=1)
-
-# Forward pass
-current_state = init_state
-states_series = []
-for current_input in inputs_series:
-    current_input = tf.reshape(current_input, [batch_size, 1])
-    input_and_state_concatenated = tf.concat([current_input, current_state], 1)  # Increasing number of columns ，在第二个维度上连接
-
-    next_state = tf.tanh(tf.matmul(input_and_state_concatenated, W) + b)  # Broadcasted addition； matmul 矩阵乘法
-    states_series.append(next_state)
-    current_state = next_state
-
-logits_series = [tf.matmul(state, W2) + b2 for state in states_series] #Broadcasted addition
-predictions_series = [tf.nn.softmax(logits) for logits in logits_series]
-
-losses = [tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits) for logits, labels in zip(logits_series,labels_series)]
-total_loss = tf.reduce_mean(losses)
-
-train_step = tf.train.AdagradOptimizer(0.3).minimize(total_loss)
-
-def plot(loss_list, predictions_series, batchX, batchY):
-    plt.subplot(2, 3, 1)
-    plt.cla()
-    plt.plot(loss_list)
-
-    for batch_series_idx in range(5):
-        one_hot_output_series = np.array(predictions_series)[:, batch_series_idx, :]
-        single_output_series = np.array([(1 if out[0] < 0.5 else 0) for out in one_hot_output_series])
-
-        plt.subplot(2, 3, batch_series_idx + 2)
-        plt.cla()
-        plt.axis([0, truncated_backprop_length, 0, 2])
-        left_offset = range(truncated_backprop_length)
-        plt.bar(left_offset, batchX[batch_series_idx, :], width=1, color="blue")
-        plt.bar(left_offset, batchY[batch_series_idx, :] * 0.5, width=1, color="red")
-        plt.bar(left_offset, single_output_series * 0.3, width=1, color="green")
-
-    plt.draw()
-    plt.pause(0.0001)
+#定义常量
+rnn_unit=10       #hidden layer units
+input_size=6
+output_size=1
+lr=0.0006         #学习率
+#――――――――――――――――――导入数据――――――――――――――――――――――
+f=open('labeled_daily_price.csv') 
+df=pd.read_csv(f)     #读入股票数据
+data=df.iloc[:,1:8].values  #取第3-10列
 
 
-with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
-    plt.ion()
-    plt.figure()
-    plt.show()
-    loss_list = []
+#获取训练集
+def get_train_data(batch_size=60,time_step=20,train_begin=0,train_end=5800):
+    batch_index=[]
+    data_train=data[train_begin:train_end]
+    normalized_train_data=(data_train-np.mean(data_train,axis=0))/np.std(data_train,axis=0)  #标准化
+    train_x,train_y=[],[]   #训练集 
+    for i in range(len(normalized_train_data)-time_step):
+       if i % batch_size==0:
+           batch_index.append(i)
+       x=normalized_train_data[i:i+time_step,:input_size]
+       y=normalized_train_data[i:i+time_step,input_size,np.newaxis]
+       train_x.append(x.tolist())
+       train_y.append(y.tolist())
+    batch_index.append((len(normalized_train_data)-time_step))
+    return batch_index,train_x,train_y
 
-    for epoch_idx in range(num_epochs):
-        x,y = generateData()
-        _current_state = np.zeros((batch_size, state_size))
 
-        print("New data, epoch", epoch_idx)
 
-        for batch_idx in range(num_batches):
-            start_idx = batch_idx * truncated_backprop_length
-            end_idx = start_idx + truncated_backprop_length
+#获取测试集
+def get_test_data(time_step=20,test_begin=5800):
+    data_test=data[test_begin:]
+    mean=np.mean(data_test,axis=0)
+    std=np.std(data_test,axis=0)
+    normalized_test_data=(data_test-mean)/std  #标准化
+    size=(len(normalized_test_data)+time_step-1)//time_step  #有size个sample 
+    test_x,test_y=[],[]  
+    for i in range(size-1):
+       x=normalized_test_data[i*time_step:(i+1)*time_step,:7]
+       y=normalized_test_data[i*time_step:(i+1)*time_step,7]
+       test_x.append(x.tolist())
+       test_y.extend(y)
+    test_x.append((normalized_test_data[(i+1)*time_step:,:7]).tolist())
+    test_y.extend((normalized_test_data[(i+1)*time_step:,7]).tolist())
+    return mean,std,test_x,test_y
 
-            batchX = x[:,start_idx:end_idx]
-            batchY = y[:,start_idx:end_idx]
 
-            _total_loss, _train_step, _current_state, _predictions_series = sess.run(
-                [total_loss, train_step, current_state, predictions_series],
-                feed_dict={
-                    batchX_placeholder:batchX,
-                    batchY_placeholder:batchY,
-                    init_state:_current_state
-                })
 
-            loss_list.append(_total_loss)
+#――――――――――――――――――定义神经网络变量――――――――――――――――――
+#输入层、输出层权重、偏置
 
-            if batch_idx%100 == 0:
-                print("Step",batch_idx, "Loss", _total_loss)
-                plot(loss_list, _predictions_series, batchX, batchY)
+weights={
+         'in':tf.Variable(tf.random_normal([input_size,rnn_unit])),
+         'out':tf.Variable(tf.random_normal([rnn_unit,1]))
+        }
+biases={
+        'in':tf.Variable(tf.constant(0.1,shape=[rnn_unit,])),
+        'out':tf.Variable(tf.constant(0.1,shape=[1,]))
+       }
 
-plt.ioff()
-plt.show()
+#――――――――――――――――――定义神经网络变量――――――――――――――――――
+def lstm(X):     
+    batch_size=tf.shape(X)[0]
+    time_step=tf.shape(X)[1]
+    w_in=weights['in']
+    b_in=biases['in']  
+    input=tf.reshape(X,[-1,input_size])  #需要将tensor转成2维进行计算，计算后的结果作为隐藏层的输入
+    input_rnn=tf.matmul(input,w_in)+b_in
+    input_rnn=tf.reshape(input_rnn,[-1,time_step,rnn_unit])  #将tensor转成3维，作为lstm cell的输入
+    cell=tf.nn.rnn_cell.BasicLSTMCell(rnn_unit)
+    init_state=cell.zero_state(batch_size,dtype=tf.float32)
+    output_rnn,final_states=tf.nn.dynamic_rnn(cell, input_rnn,initial_state=init_state, dtype=tf.float32)  #output_rnn是记录lstm每个输出节点的结果，final_states是最后一个cell的结果
+    output=tf.reshape(output_rnn,[-1,rnn_unit]) #作为输出层的输入
+    w_out=weights['out']
+    b_out=biases['out']
+    pred=tf.matmul(output,w_out)+b_out
+    return pred,final_states
+
+
+
+#――――――――――――――――――训练模型――――――――――――――――――
+def train_lstm(batch_size=80,time_step=15,train_begin=2000,train_end=5800):
+    X=tf.placeholder(tf.float32, shape=[None,time_step,input_size])
+    Y=tf.placeholder(tf.float32, shape=[None,time_step,output_size])
+    batch_index,train_x,train_y=get_train_data(batch_size,time_step,train_begin,train_end)
+    print('batch_index = {}'.format(batch_index))
+    print('train_x[0] = {}'.format(train_x[0]))
+    print('train_y[0] = {}'.format(train_y[0]))
+
+    pred,_=lstm(X)
+    #损失函数
+    loss=tf.reduce_mean(tf.square(tf.reshape(pred,[-1])-tf.reshape(Y, [-1])))
+    train_op=tf.train.AdamOptimizer(lr).minimize(loss)
+    saver=tf.train.Saver(tf.global_variables(),max_to_keep=15)
+    module_file = tf.train.latest_checkpoint("data")    
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        # saver.restore(sess, module_file)
+        #重复训练10000次
+        for i in range(2000):
+            for step in range(len(batch_index)-1):
+                _,loss_=sess.run([train_op,loss],feed_dict={X:train_x[batch_index[step]:batch_index[step+1]],Y:train_y[batch_index[step]:batch_index[step+1]]})
+            print(i,loss_)
+            # if i % 200==0:
+            #     print("保存模型：",saver.save(sess,'stock2.model',global_step=i))
+
+
+train_lstm()
+
+
+#――――――――――――――――预测模型――――――――――――――――――――
+def prediction(time_step=20):
+    X=tf.placeholder(tf.float32, shape=[None,time_step,input_size])
+    #Y=tf.placeholder(tf.float32, shape=[None,time_step,output_size])
+    mean,std,test_x,test_y=get_test_data(time_step)
+    pred,_=lstm(X)     
+    saver=tf.train.Saver(tf.global_variables())
+    with tf.Session() as sess:
+        #参数恢复
+        module_file = tf.train.latest_checkpoint()
+        saver.restore(sess, module_file) 
+        test_predict=[]
+        for step in range(len(test_x)-1):
+          prob=sess.run(pred,feed_dict={X:[test_x[step]]})   
+          predict=prob.reshape((-1))
+          test_predict.extend(predict)
+        test_y=np.array(test_y)*std[7]+mean[7]
+        test_predict=np.array(test_predict)*std[7]+mean[7]
+        acc=np.average(np.abs(test_predict-test_y[:len(test_predict)])/test_y[:len(test_predict)])  #偏差
+        #以折线图表示结果
+        plt.figure()
+        plt.plot(list(range(len(test_predict))), test_predict, color='b')
+        plt.plot(list(range(len(test_y))), test_y,  color='r')
+        plt.show()
+
+prediction() 
