1413c3de043c0492fe5a93dff59407b2aafa4fec
lzf <lzfupdate@163.com>
2017-08-02 21:41:07 +0800

    修改toothless可以被训练，但是还不能预测

--- toothless.py
+++ toothless.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 #定义常量
+model_file_path='./model/toothless.model'
 rnn_unit=10       #hidden layer units
 input_size=6
 output_size=1
@@ -15,7 +16,7 @@ data=df.iloc[:,1:8].values  #取第3-10列
 
 
 #获取训练集
-def get_train_data(batch_size=60,time_step=20,train_begin=0,train_end=5800):
+def get_train_data(batch_size=60,time_step=20,train_begin=0,train_end=2500):
     batch_index=[]
     data_train=data[train_begin:train_end]
     normalized_train_data=(data_train-np.mean(data_train,axis=0))/np.std(data_train,axis=0)  #标准化
@@ -33,7 +34,7 @@ def get_train_data(batch_size=60,time_step=20,train_begin=0,train_end=5800):
 
 
 #获取测试集
-def get_test_data(time_step=20,test_begin=5800):
+def get_test_data(time_step=20,test_begin=2500):
     data_test=data[test_begin:]
     mean=np.mean(data_test,axis=0)
     std=np.std(data_test,axis=0)
@@ -41,12 +42,12 @@ def get_test_data(time_step=20,test_begin=5800):
     size=(len(normalized_test_data)+time_step-1)//time_step  #有size个sample 
     test_x,test_y=[],[]  
     for i in range(size-1):
-       x=normalized_test_data[i*time_step:(i+1)*time_step,:7]
-       y=normalized_test_data[i*time_step:(i+1)*time_step,7]
+       x=normalized_test_data[i*time_step:(i+1)*time_step,:input_size]
+       y=normalized_test_data[i*time_step:(i+1)*time_step,input_size]
        test_x.append(x.tolist())
        test_y.extend(y)
-    test_x.append((normalized_test_data[(i+1)*time_step:,:7]).tolist())
-    test_y.extend((normalized_test_data[(i+1)*time_step:,7]).tolist())
+       test_x.append((normalized_test_data[(i+1)*time_step:,:input_size]).tolist())
+       test_y.extend((normalized_test_data[(i+1)*time_step:,input_size]).tolist())
     return mean,std,test_x,test_y
 
 
@@ -84,7 +85,7 @@ def lstm(X):
 
 
 #――――――――――――――――――训练模型――――――――――――――――――
-def train_lstm(batch_size=80,time_step=15,train_begin=2000,train_end=5800):
+def train_lstm(batch_size=80,time_step=20,train_begin=0,train_end=2500):
     X=tf.placeholder(tf.float32, shape=[None,time_step,input_size])
     Y=tf.placeholder(tf.float32, shape=[None,time_step,output_size])
     batch_index,train_x,train_y=get_train_data(batch_size,time_step,train_begin,train_end)
@@ -97,17 +98,18 @@ def train_lstm(batch_size=80,time_step=15,train_begin=2000,train_end=5800):
     loss=tf.reduce_mean(tf.square(tf.reshape(pred,[-1])-tf.reshape(Y, [-1])))
     train_op=tf.train.AdamOptimizer(lr).minimize(loss)
     saver=tf.train.Saver(tf.global_variables(),max_to_keep=15)
-    module_file = tf.train.latest_checkpoint("data")    
+    # module_file = tf.train.latest_checkpoint(model_file_path)    
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         # saver.restore(sess, module_file)
         #重复训练10000次
-        for i in range(2000):
+        for i in range(20):
             for step in range(len(batch_index)-1):
                 _,loss_=sess.run([train_op,loss],feed_dict={X:train_x[batch_index[step]:batch_index[step+1]],Y:train_y[batch_index[step]:batch_index[step+1]]})
             print(i,loss_)
             # if i % 200==0:
             #     print("保存模型：",saver.save(sess,'stock2.model',global_step=i))
+        saver.save(sess,model_file_path)
 
 
 train_lstm()
@@ -118,7 +120,7 @@ def prediction(time_step=20):
     X=tf.placeholder(tf.float32, shape=[None,time_step,input_size])
     #Y=tf.placeholder(tf.float32, shape=[None,time_step,output_size])
     mean,std,test_x,test_y=get_test_data(time_step)
-    pred,_=lstm(X)     
+    pred,_=lstm(X)   
     saver=tf.train.Saver(tf.global_variables())
     with tf.Session() as sess:
         #参数恢复
@@ -129,8 +131,8 @@ def prediction(time_step=20):
           prob=sess.run(pred,feed_dict={X:[test_x[step]]})   
           predict=prob.reshape((-1))
           test_predict.extend(predict)
-        test_y=np.array(test_y)*std[7]+mean[7]
-        test_predict=np.array(test_predict)*std[7]+mean[7]
+        test_y=np.array(test_y)*std[input_size]+mean[input_size]
+        test_predict=np.array(test_predict)*std[input_size]+mean[input_size]
         acc=np.average(np.abs(test_predict-test_y[:len(test_predict)])/test_y[:len(test_predict)])  #偏差
         #以折线图表示结果
         plt.figure()
