fe21a12ac236266115f25d24f7f0909c01ec93fa
lzf <lzfupdate@163.com>
2017-08-03 23:13:20 +0800

    fix-bug BasicLSTMCell需要有scope

--- toothless.py
+++ toothless.py
@@ -65,7 +65,7 @@ biases={
        }
 
 #――――――――――――――――――定义神经网络变量――――――――――――――――――
-def lstm(X):     
+def lstm(X, action_name):     
     batch_size=tf.shape(X)[0]
     time_step=tf.shape(X)[1]
     w_in=weights['in']
@@ -73,14 +73,15 @@ def lstm(X):
     input=tf.reshape(X,[-1,input_size])  #需要将tensor转成2维进行计算，计算后的结果作为隐藏层的输入
     input_rnn=tf.matmul(input,w_in)+b_in
     input_rnn=tf.reshape(input_rnn,[-1,time_step,rnn_unit])  #将tensor转成3维，作为lstm cell的输入
-    cell=tf.nn.rnn_cell.BasicLSTMCell(rnn_unit)
-    init_state=cell.zero_state(batch_size,dtype=tf.float32)
-    output_rnn,final_states=tf.nn.dynamic_rnn(cell, input_rnn,initial_state=init_state, dtype=tf.float32)  #output_rnn是记录lstm每个输出节点的结果，final_states是最后一个cell的结果
-    output=tf.reshape(output_rnn,[-1,rnn_unit]) #作为输出层的输入
-    w_out=weights['out']
-    b_out=biases['out']
-    pred=tf.matmul(output,w_out)+b_out
-    return pred,final_states
+    with tf.variable_scope(action_name):
+        cell=tf.nn.rnn_cell.BasicLSTMCell(rnn_unit)
+        init_state=cell.zero_state(batch_size,dtype=tf.float32)
+        output_rnn,final_states=tf.nn.dynamic_rnn(cell, input_rnn,initial_state=init_state, dtype=tf.float32)  #output_rnn是记录lstm每个输出节点的结果，final_states是最后一个cell的结果
+        output=tf.reshape(output_rnn,[-1,rnn_unit]) #作为输出层的输入
+        w_out=weights['out']
+        b_out=biases['out']
+        pred=tf.matmul(output,w_out)+b_out
+        return pred,final_states
 
 
 
@@ -93,7 +94,7 @@ def train_lstm(batch_size=80,time_step=20,train_begin=0,train_end=2500):
     print('train_x[0] = {}'.format(train_x[0]))
     print('train_y[0] = {}'.format(train_y[0]))
 
-    pred,_=lstm(X)
+    pred,_=lstm(X, "train")
     #损失函数
     loss=tf.reduce_mean(tf.square(tf.reshape(pred,[-1])-tf.reshape(Y, [-1])))
     train_op=tf.train.AdamOptimizer(lr).minimize(loss)
@@ -118,14 +119,12 @@ train_lstm()
 #――――――――――――――――预测模型――――――――――――――――――――
 def prediction(time_step=20):
     X=tf.placeholder(tf.float32, shape=[None,time_step,input_size])
-    #Y=tf.placeholder(tf.float32, shape=[None,time_step,output_size])
     mean,std,test_x,test_y=get_test_data(time_step)
-    pred,_=lstm(X)   
+    pred,_=lstm(X, "predict")
     saver=tf.train.Saver(tf.global_variables())
     with tf.Session() as sess:
         #参数恢复
-        module_file = tf.train.latest_checkpoint()
-        saver.restore(sess, module_file) 
+        saver.restore(sess, model_file_path) 
         test_predict=[]
         for step in range(len(test_x)-1):
           prob=sess.run(pred,feed_dict={X:[test_x[step]]})   
