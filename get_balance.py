4b5c1c58e0b385d3ba690b58506202bf0170435e
lzf <lzfupdate@163.com>
2017-08-20 17:27:06 +0800

    完成get_balance.py

--- get_balance.py
+++ get_balance.py
@@ -1,12 +1,17 @@
 import urllib.request
 import json
+import os.path
 
-saveFilePath = '/data/balance_origin.txt'
+saveFilePath = './data/balance_origin.txt'
 
 def get_first_in_index(list_src, list_target):
 	for i in range(len(list_src)):
-		if list_src[i] in list_target:
-			return i
+		_ = list_src[i]
+		for j in range(len(list_target)):
+			_j = list_target[j]
+			_j = _j[:len(_j) - 1]
+			if _ == _j:
+				return i
 	return -1
 
 
@@ -19,31 +24,43 @@ def get_strings(strFrom):
 
 def saveFile(dataStr):
 	if dataStr == None:
-		return False
+		return 0
 
-	with open(saveFilePath, 'ar') as targetFile:
-		linesToSave = get_strings(dataStr, ',')
-		dataInFileStr = targetFile.read()
+	print("file exist >>> {}".format(os.path.isfile(saveFilePath)))
+	if not os.path.isfile(saveFilePath):
+		f = open(saveFilePath, "a")
+		f.close()
 
-		linesInFile = targetFile.readlines()
+	with open(saveFilePath, 'r') as targetFile:
+		linesInFile = list(targetFile.readlines())
+		print('saveFile >>> len(linesInFile) = {}'.format(len(linesInFile)))
+
+	with open(saveFilePath, 'a+') as targetFile:
+		linesToSave = get_strings(dataStr)
+		print('saveFile >>> len(lineToSave) = {}'.format(len(linesToSave)))
 
 		index = get_first_in_index(linesToSave, linesInFile)
+		print('saveFile >>> index = {}'.format(index))
 		if index < 0:
-			targetFile.writelines(linesToSave)
-			return True
+			for i in range(len(linesToSave)):
+				targetFile.write(linesToSave[i])
+				targetFile.write("\n")
 		else:
 			targetFile.writelines(linesToSave[:index])
-			return False
+		return index
 
 
 
 
 page = 1
-url = 'http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx?type=FD&sty=SHSZHSSUM&st=0&sr=1&p={}&ps=50&js=var%20RQLTVKNf={pages:(pc),data:[(x)]}&rt=50073233'
-shouleContinue = True
-while shouleContinue:
-	dataStr = urllib.request.urlopen(url.format(page))
-	dataStr = '{\"datas\":' + dataStr[dataStr.index('[') + 1: (len(dataStr))]
-	print('subDataStr = {}'.format(dataStr))
-	shouldContinue = saveFile(dataStr)
+url_seg = 'http://datainterface.eastmoney.com/EM_DataCenter/JS.aspx?type=FD&sty=SHSZHSSUM&st=0&sr=1&p='
+url_seg1 = '&ps=50&js=var%20RQLTVKNf={pages:(pc),data:[(x)]}&rt=50073233'
+first_in_index = -1;
+while first_in_index < 0:
+	dataStr = urllib.request.urlopen(url_seg + str(page) + url_seg1).read().decode('utf-8')
+	print('dataStr >>> {}' + dataStr)
+	dataStr = '{\"datas\":[' + dataStr[dataStr.index('[') + 1: (len(dataStr))]
+	print('after processed, dataStr >>> ' + dataStr)
+	first_in_index = saveFile(dataStr)
+	print('shouldContinue = ' + str(first_in_index < 0))
 	page+=1
