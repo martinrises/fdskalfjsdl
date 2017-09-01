e372d2341d0601a3140ac9f49b3e455e733370c6
liuzhf <liuzhf@iskyu.com>
2017-09-01 14:58:34 +0800

    将gold_centers和gold_distances保存到csv文件中

--- toothless.py
+++ toothless.py
@@ -1,7 +1,8 @@
 from market_state_checker import MarketStateChecker
 import market_state_checker
 import random
-import math
+import os
+import csv
 
 DAYS = 5
 CENTER_NUM = 100
@@ -10,6 +11,7 @@ RANDOM_MIM = 0.98
 DISTANCE_THRESHOLD = 0.05
 GOLD_WIN = 0.003
 GOLD_NUM = 30
+MAX_EPOCH = 10
 
 class DailyRecord:
     def __init__(self, date, open, close, high, low, turnover, volume, balance):
@@ -96,141 +98,162 @@ with open("./data/daily_price.csv", "r") as src_file:
         date = words[date_ix]
         records.append(DailyRecord(date, words[open_ix], words[close_ix], words[high_ix], words[low_ix], words[turnover_ix], words[volume_ix], finance_balance.get(date)))
 
-    split_index = int(len(records) * 0.7)
-    train_records = records[:split_index]
-    test_records = records[split_index:]
-
-    k_means_records = []
-    for i in range(DAYS, len(train_records) - DAYS):
-        k_means_records.append(KMeansRecord(train_records, index=i, days=DAYS))
-
-    centers = []
-    feature_num = len(k_means_records[0].features)
-    for _ in range(CENTER_NUM):
-        points = []
-        for __ in range(feature_num):
-            points.append(random.uniform(RANDOM_MIM, RANDOM_MAX))
-        centers.append(points)
-
-    assert(len(centers) == CENTER_NUM and len(centers[0]) == feature_num and feature_num > 0)
+    split_index_1 = int(len(records) * 0.6)
+    split_index_2 = int(len(records) * 0.9)
+    train_records = records[:split_index_1]
+    cv_records = records[split_index_1: split_index_2]
+    test_records = records[split_index_2:]
 
 
-    while True:
-        # calculate distance
+    gold_centers = []
+    gold_distances = []
+    for _ in range(MAX_EPOCH):
+        # train
+        k_means_records = []
+        for i in range(DAYS, len(train_records) - DAYS):
+            k_means_records.append(KMeansRecord(train_records, index=i, days=DAYS))
+
+        centers = []
+        feature_num = len(k_means_records[0].features)
+        for _ in range(CENTER_NUM):
+            points = []
+            for __ in range(feature_num):
+                points.append(random.uniform(RANDOM_MIM, RANDOM_MAX))
+            centers.append(points)
+
+        assert(len(centers) == CENTER_NUM and len(centers[0]) == feature_num and feature_num > 0)
+
+
+        while True:
+            # calculate distance
+            for k_means_record in k_means_records:
+                distances = get_distance(k_means_record.features, centers)
+                k_means_record.center_index = distances.index(min(distances))
+
+            # update centers
+            center_cnt = {}
+            center_vecs = []
+            for _ in range(len(centers)):
+                center_vecs.append([0] * feature_num)
+
+            for k_means_record in k_means_records:
+                center_vecs[k_means_record.center_index] = list(map(lambda a,b: a+b, center_vecs[k_means_record.center_index], k_means_record.features))
+                cnt = center_cnt.get(k_means_record.center_index) if center_cnt.get(k_means_record.center_index) != None else 0
+                cnt+=1
+                center_cnt[k_means_record.center_index] = cnt
+
+            temp_centers = []
+            for i in range(len(centers)):
+                if center_cnt.get(i) != None:
+                    temp_centers.append(list(map(lambda sum: sum / center_cnt.get(i), center_vecs[i])))
+
+            # check whether centers changed
+            if temp_centers == centers:
+                break;
+            else:
+                centers = temp_centers
+
+        center_cnt = {}
+        distribute_distances = {}
         for k_means_record in k_means_records:
+            index = k_means_record.center_index
+            distance = get_distance_from_center(k_means_record.features, centers[index])
+            distribute_dis = distribute_distances.get(index) if distribute_distances.get(index) != None else 0
+            distribute_distances[index] = distribute_dis + distance
+
+            cnt = center_cnt.get(index) if center_cnt.get(index) != None else 0
+            cnt += 1
+            center_cnt[index] = cnt
+
+        average_distance = {}
+        distances_keys = distribute_distances.keys()
+        for key in distances_keys:
+            average_distance[key] = distribute_distances.get(key) / center_cnt.get(key)
+
+        center_real_cnt = {}
+        gains = {}
+        for k_means_record in k_means_records:
+            index = k_means_record.center_index
+            distance = get_distance_from_center(k_means_record.features, centers[index])
+
+            if distance < distribute_distances.get(index) * DISTANCE_THRESHOLD:
+                cnt = center_real_cnt.get(index) if center_real_cnt.get(index) != None else 0
+                cnt += 1
+                center_real_cnt[index] = cnt
+
+                gain = gains.get(index) if gains.get(index) != None else 0
+                gains[index] = gain + k_means_record.gain
+
+        for key in gains.keys():
+            gain = gains.get(key)
+            real_cnt = center_real_cnt.get(key)
+            gains[key] = gain/real_cnt
+
+        print("centers.size = {}".format(len(centers)))
+        print("centers = {}".format(centers))
+        print("center_cnt = {}".format(sort_dictionary(center_cnt)))
+        print("cneter_real_cnt = {}".format(sort_dictionary(center_real_cnt)))
+        print("gains = {}".format(sort_dictionary(gains)))
+
+        # cross validation
+        k_means_cv_records = []
+        for i in range(DAYS, len(cv_records) - DAYS):
+            k_means_cv_records.append(KMeansRecord(cv_records, index=i, days=DAYS))
+
+        cv_center_real_cnt = {}
+        cv_gains = {}
+        cv_center_cnt = {}
+        for k_means_record in k_means_cv_records:
             distances = get_distance(k_means_record.features, centers)
             k_means_record.center_index = distances.index(min(distances))
 
-        # update centers
-        center_cnt = {}
-        center_vecs = []
-        for _ in range(len(centers)):
-            center_vecs.append([0] * feature_num)
+            index = k_means_record.center_index
+            distance = distances[index]
 
-        for k_means_record in k_means_records:
-            center_vecs[k_means_record.center_index] = list(map(lambda a,b: a+b, center_vecs[k_means_record.center_index], k_means_record.features))
-            cnt = center_cnt.get(k_means_record.center_index) if center_cnt.get(k_means_record.center_index) != None else 0
-            cnt+=1
-            center_cnt[k_means_record.center_index] = cnt
+            cnt = cv_center_cnt.get(index) if cv_center_cnt.get(index) != None else 0
+            cnt += 1
+            cv_center_cnt[index] = cnt
 
-        temp_centers = []
+            if distance < distribute_distances.get(index) * DISTANCE_THRESHOLD:
+                cnt = cv_center_real_cnt.get(index) if cv_center_real_cnt.get(index) != None else 0
+                cnt += 1
+                cv_center_real_cnt[index] = cnt
+
+                gain = cv_gains.get(index) if cv_gains.get(index) != None else 0
+                cv_gains[index] = gain + k_means_record.gain
+
+        for key in cv_gains.keys():
+            gain = cv_gains.get(key)
+            real_cnt = cv_center_real_cnt.get(key)
+            cv_gains[key] = gain / real_cnt
+
+        print("\ncenters = {}".format(centers))
+        print("center_cnt = {}".format(sort_dictionary(cv_center_cnt)))
+        print("cneter_real_cnt = {}".format(sort_dictionary(cv_center_real_cnt)))
+        print("gains = {}".format(sort_dictionary(cv_gains)))
+
+        # gold centers
         for i in range(len(centers)):
-            if center_cnt.get(i) != None:
-                temp_centers.append(list(map(lambda sum: sum / center_cnt.get(i), center_vecs[i])))
+            center = centers[i]
+            if center in gold_centers:
+                continue
 
-        # check whether centers changed
-        if temp_centers == centers:
-            break;
-        else:
-            centers = temp_centers
+            if gains.get(i) != None and gains.get(i) > GOLD_WIN and cv_gains.get(i) != None and cv_gains.get(i) > GOLD_WIN and center_real_cnt.get(i) and center_real_cnt.get(i) != None and center_real_cnt.get(i) > GOLD_NUM and cv_center_real_cnt.get(i) != None and cv_center_real_cnt.get(i) > GOLD_NUM:
+                gold_centers.append(center)
+                gold_distances.append(distribute_distances.get(i))
 
-    center_cnt = {}
-    distribute_distances = {}
-    for k_means_record in k_means_records:
-        index = k_means_record.center_index
-        distance = get_distance_from_center(k_means_record.features, centers[index])
-        distribute_dis = distribute_distances.get(index) if distribute_distances.get(index) != None else 0
-        distribute_distances[index] = distribute_dis + distance
+        print("\n step # {}, gold_centers.size = {}\n\n".format(_, len(gold_centers)))
 
-        cnt = center_cnt.get(index) if center_cnt.get(index) != None else 0
-        cnt += 1
-        center_cnt[index] = cnt
-
-    average_distance = {}
-    distances_keys = distribute_distances.keys()
-    for key in distances_keys:
-        average_distance[key] = distribute_distances.get(key) / center_cnt.get(key)
-
-    center_real_cnt = {}
-    gains = {}
-    for k_means_record in k_means_records:
-        index = k_means_record.center_index
-        distance = get_distance_from_center(k_means_record.features, centers[index])
-
-        if distance < distribute_distances.get(index) * DISTANCE_THRESHOLD:
-            cnt = center_real_cnt.get(index) if center_real_cnt.get(index) != None else 0
-            cnt += 1
-            center_real_cnt[index] = cnt
-
-            gain = gains.get(index) if gains.get(index) != None else 0
-            gains[index] = gain + k_means_record.gain
-
-    for key in gains.keys():
-        gain = gains.get(key)
-        real_cnt = center_real_cnt.get(key)
-        gains[key] = gain/real_cnt
-
-    print("centers.size = {}".format(len(centers)))
-    print("centers = {}".format(centers))
-    print("center_cnt = {}".format(sort_dictionary(center_cnt)))
-    print("cneter_real_cnt = {}".format(sort_dictionary(center_real_cnt)))
-    print("gains = {}".format(sort_dictionary(gains)))
-
-    k_means_test_records = []
-    for i in range(DAYS, len(test_records) - DAYS):
-        k_means_test_records.append(KMeansRecord(test_records, index=i, days=DAYS))
-
-    test_center_real_cnt = {}
-    test_gains = {}
-    test_center_cnt = {}
-    test_center_cnt = {}
-    for k_means_record in k_means_test_records:
-        distances = get_distance(k_means_record.features, centers)
-        k_means_record.center_index = distances.index(min(distances))
-
-
-        index = k_means_record.center_index
-        distance = get_distance_from_center(k_means_record.features, centers[index])
-
-        cnt = test_center_cnt.get(index) if test_center_cnt.get(index) != None else 0
-        cnt += 1
-        test_center_cnt[index] = cnt
-
-        if distance < distribute_distances.get(index) * DISTANCE_THRESHOLD:
-            cnt = test_center_real_cnt.get(index) if test_center_real_cnt.get(index) != None else 0
-            cnt += 1
-            test_center_real_cnt[index] = cnt
-
-            gain = test_gains.get(index) if test_gains.get(index) != None else 0
-            test_gains[index] = gain + k_means_record.gain
-
-    for key in test_gains.keys():
-        gain = test_gains.get(key)
-        real_cnt = test_center_real_cnt.get(key)
-        test_gains[key] = gain/real_cnt
-
-    print("\ncenters = {}".format(centers))
-    print("center_cnt = {}".format(sort_dictionary(test_center_cnt)))
-    print("cneter_real_cnt = {}".format(sort_dictionary(test_center_real_cnt)))
-    print("gains = {}".format(sort_dictionary(test_gains)))
-
-    gold_centers = []
-    for i in range(10):
-        center = centers[i]
-        if center in gold_centers:
-            continue
-
-        if gains.get(i) != None and gains.get(i) > GOLD_WIN and test_gains.get(i) != None and test_gains.get(i) > GOLD_WIN and center_real_cnt.get(i) and center_real_cnt.get(i) != None and center_real_cnt.get(i) > GOLD_NUM and test_center_real_cnt.get(i) != None and test_center_real_cnt.get(i) > GOLD_NUM:
-            gold_centers.append(center)
 
     print("\ngold_centers.size = {}".format(len(gold_centers)))
+
+    centers_file_name = "./data/k_means/centers_" + str(DAYS) + ".csv"
+    distances_file_name = "./data/k_means/distance_" + str(DAYS) + ".csv"
+    is_file_exist = os.path.isfile(centers_file_name)
+    open_mode = "a" if is_file_exist else "w"
+    with open(centers_file_name, open_mode, newline='') as centers_f, open(distances_file_name, open_mode) as distances_f:
+        center_f_csv_writer = csv.writer(centers_f)
+        for center in gold_centers:
+            center_f_csv_writer.writerow(center)
+        for distance in gold_distances:
+            distances_f.write(str(distance) + "\n")
